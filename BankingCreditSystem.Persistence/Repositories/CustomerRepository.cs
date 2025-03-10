9c2042c192e937ec4c76b24f2530556a021d630a
engokhangok <engokhangokmail.com>
2025-03-10 13:11:16 +0300

    feat: Implement repositories for Individual and Corporate Customers
    - Added CustomerRepository base class for shared repository logic.
    - Implemented IndividualCustomerRepository and CorporateCustomerRepository.
    - Configured entity relationships and properties using EntityConfigurations.
    - Updated BaseDbContext to manage DbSet for Customer, IndividualCustomer, and CorporateCustomer.
    - Applied Table Per Type (TPT) inheritance strategy for Customer entities.

---
+++ BankingCreditSystem.Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingCreditSystem.Persistence.Repositories
+{
+    public class CustomerRepository<T> : EfRepositoryBase<T, Guid, BaseDbContext>, ICustomerRepository<T>
+        where T : Customer
+    {
+        public CustomerRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
