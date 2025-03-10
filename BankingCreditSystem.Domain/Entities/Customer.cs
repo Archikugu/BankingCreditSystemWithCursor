44f2283cd5f5ba660563471e55c0d33997002c83
engokhangok <engokhangokmail.com>
2025-03-10 11:54:44 +0300

    Add project files.

---
+++ BankingCreditSystem.Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+using System;
+using BankingCreditSystem.Core.Repositories;
+
+public abstract class Customer : Entity<Guid>
+{
+    public string PhoneNumber { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Address { get; set; } = default!;
+    public bool IsActive { get; set; }
+    
+    protected Customer()
+    {
+        IsActive = true;
+    }
+} 
